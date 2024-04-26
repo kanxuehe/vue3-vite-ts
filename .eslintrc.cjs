61f2945d8b8e0d415c88ed1ae381bbdefdeb5113
kxh <K_xuehe@163.com>
2024-04-26 11:14:27 +0800

    airbnb配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,19 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+// require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended' // 放在最后
+    // 'eslint:recommended',
+    // '@vue/eslint-config-typescript',
+    // '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'prettier/prettier': 'error'
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+const a = '123'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You a" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/"> Home </RouterLink>
+        <RouterLink to="/about"> About </RouterLink>
       </nav>
     </div>
   </header>
